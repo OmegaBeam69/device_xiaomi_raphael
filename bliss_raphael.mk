37bd4d2a1342a5c790872bf1d6e5ac54f9db1305
OmegaBeam69 <wasik15-1156@diu.edu.bd>
2025-04-14 13:43:34 +0600

    Blissify

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -15,8 +15,8 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Gapps
 #TARGET_BUILD_GAPPS := true
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_PROVIDES_POWERHAL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_raphael
+PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
