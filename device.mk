100cad6db95ff8c383a69f401bfe1bddcc9bb2ab
OmegaBeam69 <wasik15-1156@diu.edu.bd>
2025-04-16 22:29:41 +0600

    dropped android.hardware.light-service.xiaomi

--- device.mk
+++ device.mk
@@ -370,9 +370,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
 
 # Lineage Health
 PRODUCT_PACKAGES += \
