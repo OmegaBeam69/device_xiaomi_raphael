e34770a44e909bb5c5dcfc7d1caa0a5d59d4ff32
OmegaBeam69 <wasik15-1156@diu.edu.bd>
2025-05-05 10:45:11 +0600

    removed offending entries

--- device.mk
+++ device.mk
@@ -397,13 +397,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
     libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libsfplugin_ccodec_utils.vendor \
